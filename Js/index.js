5bf0997c879b457f6ef76fc07161a7b45da15b8d
Musah Ibrahim Ali <musahibrahimali@gmail.com>
2020-09-27 02:26:59 -0700

    added the store data in real time and added markers using the JavaScript google maps api

--- Js/index.js
+++ Js/index.js
@@ -1,3 +1,11 @@
+window.onload = () => {
+    displayStores();
+}
+
+let map;
+let markers = [];
+let infowindow;
+
 function initMap() {
   let losangeles = {
     lat: 34.052235,
@@ -9,4 +17,62 @@ function initMap() {
     zoom: 11,
     mapTypeId: "roadmap",
   });
+  infowindow = new google.maps.InfoWindow();
+  showStoresMarkers();
+}
+
+function displayStores(){
+    let storesHtml = '';
+    for (let [index, store] of stores.entries()){
+        let address = store['addressLines'];
+        let phone = store['phoneNumber'];
+        storesHtml += `
+            <div class="store-container">
+                <div class="store-info-container">
+                    <div class="store-address">
+                        <span>${address[0]}</span>
+                        <span>${address[1]}</span>
+                    </div>
+                    <div class="store-phone-number">
+                        ${phone}
+                    </div>
+                </div>
+                <div class="store-number-container">
+                    <div class="store-number">
+                        ${++index}
+                    </div>
+                </div>
+            </div>
+        `
+        document.querySelector('.stores-list').innerHTML = storesHtml;
+    }
+}
+
+function showStoresMarkers(){
+    let bounds = new google.maps.LatLngBounds();
+    for (let [index, store] of stores.entries()){
+        let latlng = new google.maps.LatLng(
+            store["coordinates"]["latitude"],
+            store["coordinates"]["longitude"],
+        );
+        let name = store['name'];
+        let address = store['addressLines'][0];
+        bounds.extend(latlng);
+        createMarker(latlng, name, address, ++index);
+    }
+    map.fitBounds(bounds);
+}
+
+function createMarker(latlng, name, address, index){
+    let html = "<br>" + name + "</br> <br/>" + address;
+    let marker = new google.maps.Marker({
+        map: map,
+        position: latlng,
+        label: index.toString(),
+    });
+    google.maps.event.addListener(marker, 'click', function(){
+        infowindow.setContent(html);
+        infowindow.open(map, marker);
+    });
+    markers.push(marker);
 }
